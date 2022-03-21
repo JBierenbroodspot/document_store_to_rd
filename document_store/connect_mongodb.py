e6f19de6f924767eba4e3c1c233bc6195575bc9c
Jbierenbroodspot <jim12.brood@gmail.com>
2022-03-21 14:46:13 +0100

    Add logging  - Make use of dotenv optional

--- document_store/connect_mongodb.py
+++ document_store/connect_mongodb.py
@@ -2,33 +2,35 @@
 #  SPDX-License-Identifier: BSD 3-Clause
 #  Copyright (c) 2022 Jimmy Bierenbroodspot.
 # ------------------------------------------------------------------------------
-import typing
+import os
+import logging
+
 import pymongo
 from pymongo import database
 from pymongo import errors
 import dotenv
-import os
 
 
-def main() -> None:
+def connect_to_mongodb(hostname: str | None = None, port: str | None = None) -> None:
     mongo_client: pymongo.MongoClient
-    mongo_database: pymongo.database.Database | None = None
-    connection_string: str = f"mongodb://{MONGO_HOST}:{MONGO_PORT}"
 
-    print("Connecting to database...")
+    mongo_port: str = port if port else os.getenv("MONGODB_PORT")
+    mongo_host: str = hostname if hostname else os.getenv("MONGODB_HOSTNAME")
+
+    mongo_database: pymongo.database.Database | None = None
+    connection_string: str = f"mongodb://{mongo_host}:{mongo_port}"
+
+    logging.info("Connecting to database...")
 
     try:
         mongo_client = pymongo.MongoClient(connection_string)
         mongo_database = mongo_client.op_is_op
-    except pymongo.errors.ConnectionFailure:
-        print("Connection failed")
+    except pymongo.errors.ConnectionFailure as err:
+        logging.error("Connection failed", err)
 
-    print(f"Connected to database {mongo_database.name}!")
+    logging.info(f"Connected to database {mongo_database.name}!")
 
 
 if __name__ == '__main__':
     dotenv.load_dotenv()
-    MONGO_HOST = os.getenv("MONGODB_HOSTNAME")
-    MONGO_PORT = os.getenv("MONGODB_PORT")
-
-    main()
+    connect_to_mongodb()
