834c11e034dd2bfcc26ec083f87dd42bab11e257
Jbierenbroodspot <jim12.brood@gmail.com>
2022-03-21 23:13:57 +0100

    Turn mongodb controller into class

--- document_store/mongodb_controller.py
+++ document_store/mongodb_controller.py
@@ -6,42 +6,52 @@
 things"""
 import os
 import logging
+import typing
 
 import pymongo
 from pymongo import database
 from pymongo import errors
 
+import definitions
 
-def connect_to_mongodb(hostname: str | None = None,
-                       port: str | None = None,
-                       db_name: str | None = None) -> pymongo.database.Database:
-    """Connects to a MongoDB database.
 
-    :param hostname: The name of the host that provides your database.
-    :param port: The port to your database provider (27017 if you're using localhost).
-    :param db_name: The name of the database you want to select.
-    :return: A pymongo.database.Database object containing
-    """
-    mongo_client: pymongo.MongoClient
-    mongo_database: pymongo.database.Database
+class MongoDBController:
+    """Controller for connecting and read operations for a MongoDB database."""
+    __metaclass__ = definitions.Singleton
 
-    mongo_port: str = port if port else os.getenv('MONGODB_PORT')
-    mongo_host: str = hostname if hostname else os.getenv('MONGODB_HOSTNAME')
-    mongo_db_name: str = db_name if db_name else os.getenv('DATABASE_NAME')
+    database: pymongo.database.Database
+    client: pymongo.MongoClient
+    connection_string: str
 
-    connection_string: str = f'mongodb://{mongo_host}:{mongo_port}'
+    def __init__(self, hostname: str | None = None, port: str | None = None) -> None:
+        """Initializes connection string for MongoDB database connection."""
+        mongo_port: str = port if port else os.getenv('MONGODB_PORT')
+        mongo_host: str = hostname if hostname else os.getenv('MONGODB_HOSTNAME')
 
-    logging.info('Connecting to database...')
+        self.connection_string: str = f'mongodb://{mongo_host}:{mongo_port}'
 
-    # Try connecting to the database
-    try:
-        mongo_client = pymongo.MongoClient(connection_string)
-        mongo_database = mongo_client.get_database(mongo_db_name)
-    except pymongo.errors.ConnectionFailure as err:
-        # Catch a connection error, log it and throw the error again because program should seize when this happens.
-        logging.error('Connection failed', err)
-        raise(pymongo.errors.ConnectionFailure(err))
+    def connect(self) -> pymongo.MongoClient:
+        """Connects to a MongoDB client.
 
-    logging.info(f'Connected to database {mongo_database.name}!')
+        :return: A MongoClient instance if connection was successful.
+        """
+        logging.info('Connecting to database...')
 
-    return mongo_database
+        try:
+            self.client = pymongo.MongoClient(self.connection_string)
+        except pymongo.errors.ConnectionFailure as err:
+            logging.error('Connection failed', err)
+            raise (pymongo.errors.ConnectionFailure(err))
+
+        return self.client
+
+    def set_current_database(self, database_name: str) -> None:
+        """Sets currently selected database."""
+        try:
+            self.database = self.client.get_database(database_name)
+        except pymongo.errors.PyMongoError as err:
+            logging.error(f'invalid database name: {database_name}.')
+            raise pymongo.errors.PyMongoError(err)
+
+        logging.info(f'selected {database_name}.')
+
